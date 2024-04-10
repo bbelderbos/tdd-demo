63a799c4fc7da41903ef5e00121eae77b536a446
Bob Belderbos <bobbelderbos@gmail.com>
2024-04-10 12:29:48 +0200

    uncommented final test and abstracted more into fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,22 @@ class Command(NamedTuple):
 
 
 @pytest.fixture
-def prepopulated_cache_file(tmp_path):
-    temp_file = tmp_path / "cache.txt"
+def temp_file(tmp_path):
+    return tmp_path / "cache.txt"
+
+
+@pytest.fixture
+def prepopulated_cache_file(temp_file):
     cmd = "import math; math.sqrt(16)"
     output = "4.0"
     store_command(cmd, output, file=temp_file)
     return Command(cmd, output, temp_file)
+
+
+@pytest.fixture
+def prepopulated_cache_file_more_commands(prepopulated_cache_file):
+    cmd = "[x * 2 for x in range(5)]"
+    output = [0, 2, 4, 6, 8]
+    file = prepopulated_cache_file.file
+    store_command(cmd, output, file=file)
+    return Command(cmd, output, file)
--- tests/test_script.py
+++ tests/test_script.py
@@ -4,33 +4,36 @@ from repl.script import run_python_command, store_command
 
 
 def test_run_addition():
-    result = run_python_command('1 + 1')
+    result = run_python_command("1 + 1")
     assert result == 2
+
 
 def test_run_listcomp():
     cmd = "[x * 2 for x in range(5)]"
     result = run_python_command(cmd)
     assert result == [0, 2, 4, 6, 8]
 
+
 def test_math_expression():
     cmd = "import math; math.sqrt(16)"
     result = run_python_command(cmd)
     assert result == "4.0"
 
+
 def test_store_command_and_output(prepopulated_cache_file):
     expected = f"{prepopulated_cache_file.cmd}\n{prepopulated_cache_file.output}\n"
     assert prepopulated_cache_file.file.read_text() == expected
 
-'''
-def test_store_commands_append(tmp_path):
-    cmd = "[x * 2 for x in range(5)]"
-    output = [0, 2, 4, 6, 8]
-    store_command(cmd, output, file=temp_file)
-    expected = dedent("""
+
+def test_store_commands_append(prepopulated_cache_file_more_commands):
+    expected = dedent(
+        """
     import math; math.sqrt(16)
     4.0
     [x * 2 for x in range(5)]
     [0, 2, 4, 6, 8]
-    """).lstrip()
-    assert temp_file.read_text() == expected
-'''
+    """
+    ).lstrip()
+
+    file_content = prepopulated_cache_file_more_commands.file.read_text()
+    assert file_content == expected
